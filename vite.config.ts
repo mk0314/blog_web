21d5fb93bf000649f26621a3132088d5b6aa9a08
mengkun822 <1197235402@qq.com>
2023-05-31 15:08:44 +0800

    优化vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/blog_web/',
     server: {
         host: '0.0.0.0',
         hmr: true,
     },
     plugins: [vue()],
+    build: {
+        outDir: 'docs',
+    },
 });
